aca71c48cd6567424ef2c94b55ced0a9878c956a
Reddamma <velasirireddamma@gmail.com>
2021-09-30 18:58:04 +0530

    Add header, footer and home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+
+// ng-bootstrap
+import {NgbPaginationModule, NgbAlertModule, NgbTooltip, NgbTooltipConfig} from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -12,7 +18,10 @@ import { FooterComponent } from './components/footer/footer.component';
     FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    NgbPaginationModule, NgbAlertModule, NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
