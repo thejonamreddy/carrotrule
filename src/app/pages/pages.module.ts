e6b825a3fde44a3c789d98f13d1e0024d952972c
Manoj Kumar Reddy Pollagari <thejonamreddy@gmail.com>
2021-11-12 22:25:32 +0530

    Refactored shared folder

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -4,8 +4,8 @@ import { CommonModule } from '@angular/common';
 import { PagesRoutingModule } from './pages-routing.module';
 import { PagesComponent } from './pages.component';
 
-import { HeaderComponent } from '../components/header/header.component';
-import { FooterComponent } from '../components/footer/footer.component';
+import { HeaderComponent } from '../shared/components/header/header.component';
+import { FooterComponent } from '../shared/components/footer/footer.component';
 import { DndDirective } from './workspace/dnd.directive';
 
 @NgModule({
